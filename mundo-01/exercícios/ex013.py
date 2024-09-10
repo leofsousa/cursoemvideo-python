70ba078ffc437ae652eac57d5173fa2045916a1d
Leonardo Sousa <leofsousa98@gmail.com>
2024-09-10 20:27:08 -0300

    Update ex013.py

--- mundo-01/exercícios/ex013.py
+++ mundo-01/exercícios/ex013.py
@@ -1,3 +1,4 @@
 salário = float(input('Qual é o salário do Funcionário? R$'))
 novo = salário + (salário * 15 / 100)
-print('Um funcionário que ganhava R${:.2f}, com 15% de aumento, passa a receber R${:.2f}'.format(salário, novo))
+#print('Um funcionário que ganhava R${:.2f}, com 15% de aumento, passa a receber R${:.2f}'.format(salário, novo))
+print(f'Um funcionário que ganhava R${salário:.2f}, passa a ganhar R${novo:.2f}')
